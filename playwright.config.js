f09cccc9fa5fbc0eef124058942694725d7a1594
shruthi <shruthi.r.3691@gmail.com>
2026-04-13 10:23:38 +0200

    Success scenario

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './tests',
 
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   workers: process.env.CI ? 5 : undefined,
 
   use: {
